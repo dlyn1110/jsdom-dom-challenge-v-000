6d6f9cc14fd3257996c6b7a76df93264cea2e4ed
Dani Lyn <dlyn1110@gmail.com>
2020-05-19 15:04:06 +0000

    Automatically backed up by Learn

---
+++ js/challenge.js
@@ -0,0 +1,6 @@
+// add timer to set to seconds
+// set constants to buttons by id
+// create functions for buttons (addCounter,minusCounter, likeNumber,pauseCounter,addComment)
+// add eventListeners with click and function arg
+// within pauseCounter all buttons should be disbaled except pause btn and button should show text "resume"
+// and restart counter and re-enable buttons.
